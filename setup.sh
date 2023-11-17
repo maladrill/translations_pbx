d52fff052f5759ae45dcdf5494348dd361f8db80
maladrill <maladrill@users.noreply.github.com>
2023-11-17 17:18:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 venv_dir="/home/translations"
+requirements_url="https://raw.githubusercontent.com/maladrill/translations_pbx/main/requirements.txt"
+script_url="https://raw.githubusercontent.com/maladrill/translations_pbx/main/process_translations.py"
 
 # Check if the venv directory exists
 if [ ! -d "$venv_dir" ]; then
@@ -10,21 +12,27 @@ if [ ! -d "$venv_dir" ]; then
     # Activate virtual environment
     source "$venv_dir/bin/activate"
 
-    # Download the requirements.txt file from GitHub
-    curl -o "$venv_dir/requirements.txt" https://raw.githubusercontent.com/maladrill/translations_pbx/main/requirements.txt
-
+    # Download requirements.txt from GitHub
+    curl -o "$requirements_file" "$requirements_url"
+    
     # Install required packages from requirements.txt
-    pip install -r "$venv_dir/requirements.txt"
+    pip install -r "$requirements_file"
 else
     # Activate existing virtual environment
     source "$venv_dir/bin/activate"
 fi
 
-# Download the Python script
-curl -o "$venv_dir/process_translations.py" https://raw.githubusercontent.com/maladrill/translations_pbx/main/process_translations.py
+# Download process_translations.py from GitHub
+curl -o "$script_file" "$script_url"
 
-# Run the script
-python "$venv_dir/process_translations.py"
+# Check if the Python script file exists
+if [ -f "$script_file" ]; then
+    # Run the script
+    python "$script_file"
+else
+    echo "Error: process_translations.py not found in $venv_dir"
+    exit 1
+fi
 
 # Deactivate the virtual environment
 deactivate
