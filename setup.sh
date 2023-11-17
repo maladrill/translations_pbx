f20f8bac102c27facb2ae07f7f34fbd1c816fe4a
maladrill <maladrill@users.noreply.github.com>
2023-11-17 17:24:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,16 +6,20 @@ script_file="$venv_dir/process_translations.py"
 requirements_url="https://raw.githubusercontent.com/maladrill/translations_pbx/main/requirements.txt"
 script_url="https://raw.githubusercontent.com/maladrill/translations_pbx/main/process_translations.py"
 
-# Check if Python3 is installed
-if ! command -v python3.6 &>/dev/null; then
-    echo "Error: Python3 is not installed."
+# Check if Python 3.6 is available
+if command -v python3.6 &>/dev/null; then
+    python_cmd="python3.6"
+elif command -v python3 &>/dev/null; then
+    python_cmd="python3"
+else
+    echo "Error: Python 3.6 or higher is required, but not found."
     exit 1
 fi
 
 # Check if venv directory exists
 if [ ! -d "$venv_dir" ]; then
-    # Create virtual environment
-    python3.6 -m venv "$venv_dir"
+    # Create virtual environment using the detected Python version
+    $python_cmd -m venv "$venv_dir"
 fi
 
 # Activate virtual environment
