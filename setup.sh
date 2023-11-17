f38d98964b2ba06593a086625005bd01e1305438
maladrill <maladrill@users.noreply.github.com>
2023-11-17 17:21:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 venv_dir="/home/translations"
+requirements_file="$venv_dir/requirements.txt"
+script_file="$venv_dir/process_translations.py"
 requirements_url="https://raw.githubusercontent.com/maladrill/translations_pbx/main/requirements.txt"
 script_url="https://raw.githubusercontent.com/maladrill/translations_pbx/main/process_translations.py"
 
-# Check if the venv directory exists
+# Check if Python3 is installed
+if ! command -v python3 &>/dev/null; then
+    echo "Error: Python3 is not installed."
+    exit 1
+fi
+
+# Check if venv directory exists
 if [ ! -d "$venv_dir" ]; then
     # Create virtual environment
     python3 -m venv "$venv_dir"
-
-    # Activate virtual environment
-    source "$venv_dir/bin/activate"
-
-    # Download requirements.txt from GitHub
-    curl -o "$requirements_file" "$requirements_url"
-    
-    # Install required packages from requirements.txt
-    pip install -r "$requirements_file"
-else
-    # Activate existing virtual environment
-    source "$venv_dir/bin/activate"
 fi
+
+# Activate virtual environment
+source "$venv_dir/bin/activate"
+
+# Download requirements.txt from GitHub
+curl -o "$requirements_file" "$requirements_url"
+
+# Install required packages from requirements.txt
+pip install -r "$requirements_file"
 
 # Download process_translations.py from GitHub
 curl -o "$script_file" "$script_url"
